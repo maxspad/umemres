1bd0ed839869ca780b8045da43244c14a99b9f12
Maxwell Spadafore <maxspad@umich.edu>
2022-06-09 11:06:29 -0400

    using grid data structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
+const toml = require("toml")
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/**/*.jpg");
     eleventyConfig.addPassthroughCopy('src/**/*.png');
     eleventyConfig.addPassthroughCopy('src/**/*.svg');
     eleventyConfig.addPassthroughCopy('src/**/*.pdf');
+
+    //eleventyConfig.addDataExtension("toml", contents => toml.parse(contents));
+    
     // Set custom directories for input, output, includes, and data
     return {
       dir: {
