f161336089c5f172a165eec93d9436d620e17bf3
Maxwell Spadafore <maxspad@umich.edu>
2022-06-17 17:00:47 -0400

    added anticoag and hstrop algorithms, removed bullet points

--- components/gridbox.js
+++ components/gridbox.js
@@ -1,5 +1,6 @@
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+import Container from "react-bootstrap/Container"
 
 export default function Gridbox(props) {
     const id = props.id; 
@@ -13,7 +14,8 @@ export default function Gridbox(props) {
                     {/* <Card.Title>{data.title}</Card.Title> */}
                     <Card.Text>
                         {props.children}
-                        <ul>
+                        <Container className="px-2">
+                        <ul className="list-unstyled">
                             {data.links.map((obj, i) => {
                                 if ((obj.enabled) || (obj.enabled == null)) {
                                     return (
@@ -25,6 +27,7 @@ export default function Gridbox(props) {
                                 }
                             })}
                         </ul>
+                        </Container>
                     </Card.Text>
                 </Card.Body>
             </Card>
