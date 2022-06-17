15ad154cf1bb5975ab3e9fad13dea08d270de468
Maxwell Spadafore <maxspad@umich.edu>
2022-06-17 16:01:22 -0400

    using cards instead of generic grid

--- components/gridbox.js
+++ components/gridbox.js
@@ -1,12 +1,34 @@
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
 export default function Gridbox(props) {
     const id = props.id; 
     const data = props.data[id];
     return (
         <>
-        <Col lg={4} className="gy-3 gx-3">
-            <div className='border bg-light p-3' style={{height: "100%"}}>
+        <Col lg={4} className="gy-3">
+            <Card style={{height: "100%"}}>
+                <Card.Header as="h2">{data.title}</Card.Header>
+                <Card.Body style={{backgroundColor: "#fcfcfc"}}>
+                    {/* <Card.Title>{data.title}</Card.Title> */}
+                    <Card.Text>
+                        {props.children}
+                        <ul>
+                            {data.links.map((obj, i) => {
+                                if ((obj.enabled) || (obj.enabled == null)) {
+                                    return (
+                                        <li>
+                                            <a href={obj.href} className="text-decoration-none" target="_blank">{obj.icon}</a>
+                                            <a href={obj.href} target="_blank">{obj.text}</a>
+                                        </li>
+                                    );
+                                }
+                            })}
+                        </ul>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            {/* <div className='border bg-light p-3' style={{height: "100%"}}>
                 <h2>{data.title}</h2>
                 {props.children}
                 <ul>
@@ -21,8 +43,13 @@ export default function Gridbox(props) {
                         }
                     })}
                 </ul>
-            </div>
-        </Col>    
+            </div> */}
+        </Col> 
+        <style jsx>{`
+                .mycard {
+                    background-color: #000000;
+                }
+            `}</style>   
         </>
     );
 }
